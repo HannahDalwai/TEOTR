58a8ac5f963cb5ff830015e55afcf0119880ed2c
HannahDalwai <91248477+HannahDalwai@users.noreply.github.com>
2021-11-26 20:41:41 -1200

    tings

--- script.js
+++ script.js
@@ -121,22 +121,5 @@ photo.onmouseout = function() {
 
 
 // projects
-$(".project-detail").slick({
-	slidesToShow: 1,
-	arrows: false,
-	asNavFor: '.project-strip',
-	autoplay: true,
-	autoplaySpeed: 3000
-});
 
-$(".project-strip").slick({
-	slidesToShow: 5,
-	slidesToScroll: 1,
-	arrows: false,
-	asNavFor: '.project-detail',
-	dots: false,
-	infinite: true,
-	centerMode: true,
-	focusOnSelect: true
-});
 // ends
