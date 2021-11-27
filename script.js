db7f99c6c82dd5610413d5037e61f90ecefc828d
HannahDalwai <91248477+HannahDalwai@users.noreply.github.com>
2021-11-26 20:15:09 -1200

    stuff

--- script.js
+++ script.js
@@ -117,3 +117,26 @@ photo.onmouseout = function() {
   this.style.transform = "scale(1,1)";
   this.style.boxShadow = "none";
 };
+
+
+
+// projects
+$(".project-detail").slick({
+	slidesToShow: 1,
+	arrows: false,
+	asNavFor: '.project-strip',
+	autoplay: true,
+	autoplaySpeed: 3000
+});
+
+$(".project-strip").slick({
+	slidesToShow: 5,
+	slidesToScroll: 1,
+	arrows: false,
+	asNavFor: '.project-detail',
+	dots: false,
+	infinite: true,
+	centerMode: true,
+	focusOnSelect: true
+});
+// ends
